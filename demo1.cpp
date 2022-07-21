2c3bb1ebde245fcbcf285d7e2d8952311a88e5b5
beiyuanfuyun <109722550+beiyuanfuyun@users.noreply.github.com>
2022-07-21 16:36:15 +0800

    Add files via upload

---
+++ demo1.cpp
@@ -0,0 +1,22 @@
+//#include<stdio.h>//包含一个叫stido.h的文件，<>必须要有，std-标准-standard i-input o-output
+//int main()//main代表主函数-fn+f10程序入口，且有且仅有一个，
+//int代表整型
+//main前面的int，标识main，mian函数调用需要返回一个整型值也就是最后的 return 0
+//{
+	//完成任务
+	//屏幕上输出helloworld
+	//函数-pritnf()-print-function函数
+	//print("helloworld/n")
+	//print函数为库函数——c语言提供的可以使用的函数
+	//别人的东西————打招呼
+	//#include
+	//printf("hello  world\n");
+	
+	//return 0;//返回,整数0
+//}
+//#include<stdio.h>
+//int main()
+//{
+	//printf("helloworld");
+	//return 0;
+//}
